078402b18f92aea53383ba7e678063a3dbcdc780
l0stfake7 <bartek.poniatyszyn@gmail.com>
2015-10-16 21:03:51 +0200

    Update 0.2.0 - main file with entry point

---
+++ ProgrammingChallenge/00NameGenerator/Main.cpp
@@ -0,0 +1,11 @@
+/*
+Features: add names, find names, generate random name from base, generate names based on user set letters
+*/
+#include <iostream>
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
