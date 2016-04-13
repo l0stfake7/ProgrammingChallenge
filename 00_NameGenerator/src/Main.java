a2cd87998967f761ac0bae4daa5cb7644b6109de
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-04-13 22:23:57 +0200

    Update 1.1.4 - fix my local repo

---
+++ 00_NameGenerator/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		Name n1 = new Name("Bartek");
+		System.out.println(n1.getName());
+		
+		MyRandom rand1 = new RandomBuilder()
+			.rangeMax(1000)
+			.rangeMin(90)
+			.charBegin('a')
+			.charEnd('e')
+			.buildRandom();
+		int x = 0;
+		while(x < 10) {
+			int testval = rand1.getInt();
+			System.out.println(testval);
+			x++;
+		}
+		
+		x = 0;
+		while(x < 10) {
+			char testval = rand1.getChar();
+			System.out.println(testval);
+			x++;
+		}
+		
+		int a = 0;
+	}
+
+}
