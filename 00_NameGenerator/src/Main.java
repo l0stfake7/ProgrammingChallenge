02d0248be370a5dcb725e9e8f13c5f1a5955cbe3
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-04-16 15:01:21 +0200

    Update 1.1.8 - ready to use

--- 00_NameGenerator/src/Main.java
+++ 00_NameGenerator/src/Main.java
@@ -1,14 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World");
-
-		NameGenerator ng = new NameGenerator('a', 'm');
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Programming Challenge - 00 - Name Generator");
+		System.out.println("Type char:");
+		char a = scanner.next().charAt(0);
+		
+		System.out.println("Type char:");
+		char b = scanner.next().charAt(0);
+		
+		NameGenerator ng = new NameGenerator(a, b);
 		System.out.println(ng.getName());
 		
 		
