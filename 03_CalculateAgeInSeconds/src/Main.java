b640111343875ec72e2877ebd2ffc7b19ddd912a
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-04-20 23:58:06 +0200

    Update 3.4.1

--- 03_CalculateAgeInSeconds/src/Main.java
+++ 03_CalculateAgeInSeconds/src/Main.java
@@ -1,12 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		
+		try {
+			System.out.println("Programming Challenge - 03 - Calculate age in second");
+					
+			Scanner scanner = new Scanner(System.in);
+			short age = 0;
+			
+			System.out.println("Type your age");
+			
+			age = scanner.nextShort();
+			
+			System.out.print("Your age: ");
+			System.out.print(age);
+			System.out.print(" is ");
+		
+			scanner.close();
+		}
+		catch(InputMismatchException ime) {
+			System.out.print(ime.getMessage());
+		}
+		catch(Exception e) {
+			System.out.print(e.getMessage());		
+		}
 	}
-
 }
